e4010481d4b63140ad392ef7d1d22437ee96d5f8
mlvzk <>
2019-11-22 15:50:15 +0100

    fix mobile links

--- BotCommands/ReplaceAliReflinkCommand.cs
+++ BotCommands/ReplaceAliReflinkCommand.cs
@@ -11,6 +11,7 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using System.Web;
 
 namespace PigBot.BotCommands
 {
@@ -23,49 +24,61 @@ namespace PigBot.BotCommands
 			return CheckSClick.IsMatch(message.Content);
 		}
 
+        private static async Task<string> DeleteRef(string content)
+        {
+            var replaceMessage = content;
+
+            var aliParser = new Regex(@"(http[s]?://s\.click)\S+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            foreach (Match match in aliParser.Matches(content))
+            {
+                try
+                {
+                    var httpClient = new HttpClient();
+                    var result = await httpClient.GetAsync(match.Value);
+                    // await message.Channel.SendMessageAsync(result.RequestMessage.ToString()); // debug
+                    // if redirectUrl is found in the 'expanded' referral link, extract the actual product url
+                    // there has to be a less potato way to do this... TODO: clean me up ;)
+                    if (result.RequestMessage.ToString().Contains("redirectUrl"))
+                    {
+                        // csgraduate.jpg
+                        var responseUrl = result.RequestMessage.RequestUri.ToString().Split("redirectUrl=").Last().Split("m%252F").Last().Split("%252F").First();
+                        // this is definitely wrong, but it works(tm)
+                        responseUrl = "https://www.aliexpress.com/item/" + responseUrl + ".html";
+                        replaceMessage = replaceMessage.Replace(match.Value, responseUrl);
+                    }
+                    else if (result.RequestMessage.RequestUri.Query.Contains("rdtUrl"))
+                    {
+                        var query = HttpUtility.ParseQueryString(result.RequestMessage.RequestUri.Query);
+                        replaceMessage = replaceMessage.Replace(match.Value, "https://www.aliexpress.com" + (new Uri(HttpUtility.UrlDecode(query["rdtUrl"]))).AbsolutePath);
+                    }
+                    else
+                    {
+                        var responseUrl = result.RequestMessage.RequestUri.ToString().Split("?").First();
+                        replaceMessage = replaceMessage.Replace(match.Value, responseUrl);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Could not replace: " + e.Message);
+                }
+            }
+
+            var aliMobileParser = new Regex(@"\b(?:https://m\.aliexpress\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            foreach (Match match in aliMobileParser.Matches(content))
+            {
+                Console.WriteLine(match.Value);
+                var cleanUrl = match.Value.Split("?").First();
+                replaceMessage = replaceMessage.Replace(match.Value, cleanUrl);
+            }
+
+            return replaceMessage;
+        }
+
 		public async Task Execute(SocketMessage message)
 		{
 			//await message.Channel.SendMessageAsync("A s.click referral link was detected!"); // debug
-			var replaceMessage = message.Content;
-
-			var aliParser = new Regex(@"(?:.*//s\.click)\S+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-			foreach (Match match in aliParser.Matches(message.Content))
-			{
-				try
-				{
-					var httpClient = new HttpClient();
-					var result = await httpClient.GetAsync(match.Value);
-					// await message.Channel.SendMessageAsync(result.RequestMessage.ToString()); // debug
-					// if redirectUrl is found in the 'expanded' referral link, extract the actual product url
-					// there has to be a less potato way to do this... TODO: clean me up ;)
-					if(result.RequestMessage.ToString().Contains("redirectUrl"))
-					{
-						// csgraduate.jpg
-						var responseUrl = result.RequestMessage.RequestUri.ToString().Split("redirectUrl=").Last().Split("m%252F").Last().Split("%252F").First();
-						// this is definitely wrong, but it works(tm)
-						responseUrl = "https://www.aliexpress.com/item/" + responseUrl + ".html";
-						replaceMessage = replaceMessage.Replace(match.Value, responseUrl);
-					}
-					else
-					{
-						var responseUrl = result.RequestMessage.RequestUri.ToString().Split("?").First();
-						replaceMessage = replaceMessage.Replace(match.Value, responseUrl);
-					}
-
-				}
-				catch (Exception e)
-				{
-					Console.WriteLine("Could not replace: "+e.Message);
-				}
-			}
-
-			var aliMobileParser = new Regex(@"\b(?:https://m\.aliexpress\.)\S+\b", RegexOptions.Compiled | RegexOptions.IgnoreCase);
-			foreach (Match match in aliMobileParser.Matches(message.Content))
-			{
-				Console.WriteLine(match.Value);
-				var cleanUrl = match.Value.Split("?").First();
-				replaceMessage = replaceMessage.Replace(match.Value, cleanUrl);
-			}
+			var replaceMessage = await DeleteRef(message.Content);
 
 			var embedBuilder = new EmbedBuilder();
 			embedBuilder.Color = Color.Magenta;
